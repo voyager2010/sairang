af9cbcdc4b2223600945db0d0ea7085b1b46a40a
voyager2010 <saihole2010@gmail.com>
2026-07-21 20:07:03 +0530

    home page Popular Destinations

---
+++ js/home.js
@@ -0,0 +1,42 @@
+const container = document.getElementById("cardContainer");
+
+let scrollAmount = 0;
+const cardWidth = 300; 
+
+setInterval(() => {
+    if (container.scrollLeft + container.clientWidth >= container.scrollWidth) {
+        
+        container.scrollTo({
+            left: 0,
+            behavior: "smooth"
+        });
+    } else {
+        
+        container.scrollBy({
+            left: cardWidth,
+            behavior: "smooth"
+        });
+    }
+}, 3000); // Slide every 3 seconds
+
+
+
+
+function changeTab(region, button){
+
+    let sections = document.querySelectorAll(".destination-box");
+
+    sections.forEach(function(section){
+        section.classList.remove("show");
+    });
+
+    document.getElementById(region).classList.add("show");
+
+    let buttons = document.querySelectorAll(".tab-btn");
+
+    buttons.forEach(function(btn){
+        btn.classList.remove("active");
+    });
+
+    button.classList.add("active");
+}
